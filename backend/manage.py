e37c999bf46bbc20d19a377bc9ef3f3d7d1eea2a
stasostroholov <stanislavostrogolov@gmail.com>
2026-04-22 12:34:24 +0300

    Refactor: removed outdated flask-script and MigrateCommand

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,13 @@
+import os
 from todo.app import create_app
 from todo.extensions import db
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
 
-migrate = Migrate(create_app, db)
+# Створюємо екземпляр додатка
+app = create_app()
 
-manager = Manager(create_app)
-manager.add_command('db', MigrateCommand)
+# Ініціалізуємо міграції для цього додатка
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
