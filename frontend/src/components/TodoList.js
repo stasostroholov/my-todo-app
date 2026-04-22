c68fea51b67cf9ce5d6c1744ebc70a384afc6893
stasostroholov <stanislavostrogolov@gmail.com>
2026-04-22 12:47:25 +0300

    front changed

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -13,7 +13,7 @@ const TodoList = ({ setTodo, filteredTodos }) => {
                     filteredTodos.map((todo) => (
                         <Todo key={todo.id} todo={todo} setTodo={setTodo} />
                     )) :
-                    <Alert variant="info">Сучка шлюха</Alert>
+                    <Alert variant="info">to do!!</Alert>
                 }
             </ListGroup>
         </div>
